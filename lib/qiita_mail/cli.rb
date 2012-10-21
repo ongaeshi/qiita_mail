09ced6cf3ea8080df0b194c9868ba8ad786dca89
ongaeshi <ongaeshi0621@gmail.com>
2012-10-21 11:46:53 +0900

    cli.rb: Add 'qiita_mail text' command

--- lib/qiita_mail/cli.rb
+++ lib/qiita_mail/cli.rb
@@ -9,7 +9,7 @@ require 'rubygems'
 require 'qiita_mail'
 require 'thor'
 require 'qiita_mail/selector'
-# require 'qiita_mail/format_text'
+require 'qiita_mail/format_text'
 require 'qiita_mail/format_html'
 require 'qiita_mail/mailer'
 
@@ -23,7 +23,8 @@ module QiitaMail
     desc "deliver", "Deliver mail."
     def deliver
       # ピックアップ
-      mail_body = pickup_and_format
+      puts "Pickup from Qiita.com ..."
+      mail_body = pickup_and_format(:html)
       
       # メールの送信
       puts "Send mail ..."
@@ -31,28 +32,52 @@ module QiitaMail
       mailer.deliver
     end
 
-    desc "file", "Write file."
-    def file(filename)
+    desc "html [filename]", "Write html."
+    def html(filename = nil)
       # ピックアップ
-      mail_body = pickup_and_format
+      puts "Pickup from Qiita.com ..."
+      mail_body = pickup_and_format(:html)
       
       # ファイルに書き込み
-      puts "Write file -> #{filename}"
-      open(filename, "w") do |f|
-        f.write mail_body
-      end
+      puts "Write html -> #{filename}"
+      write_or_puts(filename, mail_body)
+    end
+
+    desc "text [filename]", "Write text format."
+    def text(filename = nil)
+      # ピックアップ
+      puts "Pickup from Qiita.com ..."
+      mail_body = pickup_and_format(:text)
+      
+      # 標準出力に出力
+      puts "Write text -> #{filename}"
+      write_or_puts(filename, mail_body)
     end
 
     private
 
-    def pickup_and_format
+    def pickup_and_format(kind)
       # 記事をピックアップ
-      puts "Pickup from Qiita.com ..."
       selector = Selector.new("")
       pickup_items = selector.pickup(5)
 
       # テキスト整形
-      FormatHTML.new(pickup_items).to_s
+      case kind
+      when :html
+        FormatHTML.new(pickup_items).to_s
+      when :text
+        FormatText.new(pickup_items).to_s
+      end
+    end
+
+    def write_or_puts(filename, str)
+      if (filename)
+        open(filename, "w") do |f|
+          f.write str
+        end
+      else
+        puts str
+      end
     end
   end
 end
