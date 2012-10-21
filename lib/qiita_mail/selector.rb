0e97c1d51df70dc4bbae3d44b8ab4b64f3e361a0
ongaeshi <ongaeshi0621@gmail.com>
2012-10-21 14:37:14 +0900

    marshal_file.rb: API問い合わせ結果のキャッシュ
    デバッグを容易＆高速にするため

--- lib/qiita_mail/selector.rb
+++ lib/qiita_mail/selector.rb
@@ -7,8 +7,11 @@
 
 require 'rubygems'
 require 'qiita'
+require 'qiita_mail/marshal_file'
 
 module QiitaMail
+  CACHE_FILE = File.join(File.dirname(__FILE__), '../../test/data/pickup_cache.marshal')
+
   class Selector
     def initialize(token)
       @token = token
@@ -17,17 +20,30 @@ module QiitaMail
     TAGS = ['ruby', 'javascript']
 
     def pickup(num = 5)
-      items = []
-
-      TAGS.each do |tag|
-          items += Qiita.tag_items(tag)
-      end
+      # 本番用ピックアップ
+      items = pickup_items
+      # キャッシュを保存
+      # MarshalFile.save(CACHE_FILE, items)
+      # キャッシュからロード(高速)
+      # items = MarshalFile.load(CACHE_FILE)
 
       items = items.sort { |a, b|
         a.stock_count <=> b.stock_count
       }.reverse
 
       items[0..num-1]
+    end
+
+    private
+
+    def pickup_items
+      items = []
+
+      TAGS.each do |tag|
+        items += Qiita.tag_items(tag)
+      end
+
+      items
     end
   end
 end
